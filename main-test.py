8fb8e376f8215bd2c45a82fdd9e107841b656cdc
nisnym <nisnym@gmail.com>
2022-01-27 23:10:38 +0530

    added right test

--- main-test.py
+++ main-test.py
@@ -5,7 +5,7 @@ def TestAdd():
         assert Add(2,3) == 5
         assert Add(5,5) == 10
         assert Add(-1,1) == 0
-        assert Add(5,5) == 11
+        assert Add(6,6) == 12
         print("Add Function works correctly")
 
 if __name__ == '__main__':
